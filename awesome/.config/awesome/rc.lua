4466fbd8a2d928e8a4dc3da413fc5c973e0ffcb8
John <1112999+jpgleg@users.noreply.github.com>
2019-12-07 14:05:32 -0800

    notifications, tmux poking

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -49,6 +49,7 @@ end
 
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
+beautiful.notification_icon_size = 48
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
